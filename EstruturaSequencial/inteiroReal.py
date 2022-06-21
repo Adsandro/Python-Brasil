7492b2516d7b2e98017329a42d42a5c0fa667ea3
Adsandro Carvalho <adsandroxerd@gmail.com>
2022-06-21 02:19:11 -0300

    Adiciona conceito de calculos

---
+++ EstruturaSequencial/inteiroReal.py
@@ -0,0 +1,14 @@
+int1 = int(input('Informe o primeiro numero interio: '))
+int2 = int(input('informe o segundo numero inteiro: '))
+real = float(input('informe um numero do conjunto dos reais: '))
+
+calculo1 = (int1*2)*(int2/2)
+calculo2 = (int1*3)+real
+calculo3 = real**3
+
+print('O produto do dobro do primeiro com metade do segundo é {} \nA soma do triplo do primeiro com o terceiro é {} \nO terceiro elevado ao cubo é {} '.format(calculo1, calculo2, calculo3))
+
+#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+# o produto do dobro do primeiro com metade do segundo .
+# a soma do triplo do primeiro com o terceiro.
+# o terceiro elevado ao cubo.
